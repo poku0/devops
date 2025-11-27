734ffcd9e79d0a0597e9a2713386c0a44b1e84e6
Povilas <povilas@MacPro.local>
2025-11-27 21:58:05 +0200

    updated script

--- 15/script.sh
+++ 15/script.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
+# ensure main is latest 
+git checkout main 
+git pull 
+
+# create new branch
 git checkout -b feature/orange
-touch oranges
-echo "This file contains oranges" >> oranges
+
+# add file 
+echo "This file contains oranges" > oranges
+
+# commit changes
 git add oranges
 git commit -m "added oranges."
+
+# push
 git push --set-upstream origin feature/orange
 
+# merge to main
 git checkout main
 git merge feature/orange
+
+# push 
+git push 
